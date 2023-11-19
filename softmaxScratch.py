73838e095655b3b4cd5527152c05c8ed87e55467
blameitonme1 <2741122036@qq.com>
2023-11-19 17:07:25 +0800

    completed softmax from scratch, using jupyter then the animation is available

---
+++ softmaxScratch.py
@@ -0,0 +1,127 @@
+import torch
+from IPython.display import SVG, display_svg
+from d2l import torch as d2l
+# softmax也是一个单层神经网络，同时也是全连接层
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+num_inputs = 784 #展平为向量
+num_outputs = 10
+# 初始化weight和bias
+W = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
+b = torch.zeros(num_outputs, requires_grad=True)
+def softmax(x):
+    # 注意x是一个矩阵，因为是针对一个批量来说的
+    x_exp = torch.exp(x)
+    partition = x_exp.sum(1, keepdim=True)
+    return x_exp / partition
+def net(x):
+    return softmax(torch.matmul(x.reshape(-1, W.shape[0]), W) + b)
+# 实现交叉熵损失函数
+def cross_entropy(y_hat, y):
+    return -torch.log(y_hat[range(len(y_hat)), y])
+# 分类精度, 也是模型性能的一个重要指标
+def accuracy(y_hat, y):  #@save
+    """计算预测正确的数量"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+def evaluate_accuracy(net, data_iter): #@save
+    """计算在指定数据集上模型的精度"""
+    if isinstance(net, torch.nn.Module):
+        net.eval()  # 将模型设置为评估模式
+        metric = Accumulator(2)  # 正确预测数、预测总数
+        with torch.no_grad():
+            for X, y in data_iter:
+                metric.add(accuracy(net(X), y), y.numel())
+        return metric[0] / metric[1]
+# accumulator的实现，没啥好说的
+class Accumulator:  #@save
+    """在n个变量上累加"""
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+class Animator:  #@save
+    """在动画中绘制数据"""
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        d2l.use_svg_display()
+        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: d2l.set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        d2l.display.display(self.fig)
+        d2l.display.clear_output(wait=True)
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使用PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step() # 自动更新相关参数
+        else:
+            # 使用定制的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()),accuracy(y_hat, y), y.numel())
+    # 返回训练损失和训练精度
+    return metric[0] / metric[2], metric[1] / metric[2] 
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
+    """训练模型 定义见第3章"""
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        # 动画实时显示一些准确度和失误
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+        train_loss, train_acc = train_metrics
+# 手动设置updater
+lr = 0.1
+def updater(batch_size):
+    return d2l.sgd([W, b], lr, batch_size)
+num_epochs= 10
+train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
+    
