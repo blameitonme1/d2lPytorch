648d3d3af669808be13c9240062e474508ed0f37
blameitonme1 <2741122036@qq.com>
2023-12-25 22:16:54 +0800

    learned one important lesson, small learning rate when doing transfer learning!

--- classify-leaves.py
+++ classify-leaves.py
@@ -9,18 +9,25 @@ import os
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset
 from sklearn.model_selection import train_test_split
+# global variable
+cat_to_index = {}
 # change the classifier of a pretrained model
 class ModifiedVGG16(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        model = model = torchvision.models.vgg16(pretrained=True)
+        model = torchvision.models.vgg16(pretrained=True)
         self.features = model.features
+        # freeze these parameters to avoid them being updated.
+        for param in self.features.parameters():
+            param.requires_grad = False
         self.classifer = torch.nn.Sequential(
             nn.Flatten(),
-            nn.Dropout(p=0.5),
             nn.Linear(25088, 4096),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
             nn.Linear(4096, 176)
         )
     def forward(self, X):
@@ -28,7 +35,7 @@ class ModifiedVGG16(torch.nn.Module):
         X = self.classifer(X)
         return X
 
-data_dir = "D:/DeepLearning/data/classify-leaves/classify-leaves"
+data_dir = "D:\\DeepLearning\\data\\classify-leaves\\classify-leaves"
 
 class LeaveDataset(Dataset):
     def __init__(self, dataframe, transform=None) -> None:
@@ -42,11 +49,10 @@ class LeaveDataset(Dataset):
     def __getitem__(self, idx):
         relative_path = self.imgs.iloc[idx]
         img = Image.open(os.path.join(data_dir, relative_path))
-        id = relative_path.split('/')[-1].split('.')[0]
         label = self.labels.iloc[idx]
         if self.transform:
             img = self.transform(img)
-        return img, cat_to_index[label]
+        return img, torch.tensor(cat_to_index[label])
 
 class predDataset(Dataset):
     def __init__(self, dataframe, transform=None) -> None:
@@ -65,12 +71,74 @@ class predDataset(Dataset):
 train_augs = torchvision.transforms.Compose([
     torchvision.transforms.Resize(224),
     torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.RandomVerticalFlip(),
     torchvision.transforms.ToTensor(),
 ])
 test_augs = torchvision.transforms.Compose([
     torchvision.transforms.Resize(224),
     torchvision.transforms.ToTensor()
 ])
+def train_model(model, train_loader, test_loader, loss_fn, trainer, num_epochs=5, device='cuda'):
+    #assign it to model! Otherwise can't exploit GPU
+    model = model.to(device)
+    
+    for epoch in range(num_epochs):
+        print(f"epoch {epoch + 1}")
+        total_loss = 0.0
+        correct_predictions = 0
+        total_samples = 0
+
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            trainer.zero_grad()
+
+            outputs = model(inputs)
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+            trainer.step()
+
+            total_loss += loss.item()
+
+            _, predictions = torch.max(outputs.data, 1)
+            correct_predictions += (predictions == labels).sum().item()
+            total_samples += labels.size(0)
+
+        train_loss = total_loss / len(train_loader)
+        train_accuracy = correct_predictions / total_samples
+
+        print(f'Epoch {epoch + 1}/{num_epochs}, '
+              f'Training Loss: {train_loss:.4f}, '
+              f'Training Accuracy: {train_accuracy:.4f}')
+
+        # Evaluate on the test set
+        test_accuracy = test_model(model, test_loader)
+        print(f'Test Accuracy: {test_accuracy:.4f}')
+
+    print('Training finished.')
+
+def test_model(net, test_loader):
+    net = net.cuda()
+    net.eval()
+    total_correct = 0
+    total_samples = 0
+
+    with torch.no_grad():
+        for batch, labels in test_loader:
+            batch = batch.cuda()
+            labels = labels.cuda()
+            # Forward pass
+            outputs = net(batch)
+
+            # Calculate accuracy
+            _, predictions = torch.max(outputs, 1)
+            total_correct += (predictions == labels).sum().item()
+            total_samples += labels.size(0)
+
+    accuracy = (total_correct / total_samples)
+    print(f'Test Accuracy: {accuracy:.4f}')
+    return accuracy
+
 net = ModifiedVGG16().cuda()
 df1 = pd.read_csv("D:\\DeepLearning\\data\\classify-leaves\\classify-leaves\\train.csv")
 categories = df1['label'].unique().tolist()
@@ -79,14 +147,15 @@ cat_to_index = {leaf_name : index for index, leaf_name in enumerate(categories)}
 train_df, val_df = train_test_split(df1, test_size=0.2, random_state=42)
 train_dataset = LeaveDataset(train_df, transform=train_augs)
 test_dataset = LeaveDataset(val_df, transform=test_augs)
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 loss = nn.CrossEntropyLoss()
-trainer = torch.optim.Adam(net.parameters(), lr=0.001)
+trainer = torch.optim.Adam(net.parameters(), lr=0.0001)
 # finetuning a liitle bit
-d2l.train_ch13(net, train_iter=train_loader, test_iter=test_loader, loss=loss, trainer=trainer, num_epochs=5)
-
-df2 = pd.read_csv("D:\DeepLearning\data\classify-leaves\classify-leaves\test.csv")
+train_model(net, train_loader=train_loader, test_loader=test_loader, loss_fn=loss, trainer=trainer, num_epochs=5)
+# test accuracy
+test_model(net, test_loader)
+df2 = pd.read_csv("D:\\DeepLearning\\data\\classify-leaves\\classify-leaves\\test.csv")
 pred_dataset = predDataset(df2, transform=test_augs)
 pred_loader = DataLoader(pred_dataset, batch_size=64, shuffle=False)
 pred = []
@@ -96,7 +165,7 @@ with torch.no_grad():
         batch = batch.cuda()
         batch = net(batch)
         _, pred_labels = torch.max(batch, 1)
-        labels = [cat_to_index[index.item()] for index in pred_labels]
+        labels = [categories[index.item()] for index in pred_labels]
         pred.extend(list(zip(names, labels)))
 
 with open('submissions.csv', 'w', newline='') as csvfile:
