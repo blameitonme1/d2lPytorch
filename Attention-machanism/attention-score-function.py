b843dbb5b45891fc8e92896e0be6e19c9334a189
blameitonme1 <2741122036@qq.com>
2023-12-27 23:19:24 +0800

    learned two attention scoring functions

--- Attention-machanism/attention-score-function.py
+++ Attention-machanism/attention-score-function.py
@@ -2,6 +2,9 @@ import torch
 import math
 from torch import nn
 from d2l import torch as d2l
+'''
+The torch.bmm is just operate on each batch.
+'''
 def masked_softmax(X, valid_lens):
     '''
     mask some elements when calculating softmax on X. Support 1D or 2D valid_lens
@@ -30,10 +33,26 @@ class AdditiveAttention(nn.Module):
         self.dropout = nn.Dropout(dropout)
     
     def forward(self, queries, keys, values, valid_lens):
+        # need to be familiar with the api.
         queries, keys = self.w_q(queries). self.w_k(keys)
         features = queries.unsqueeze(2) + keys.unsqueeze(1)
         features = torch.tanh(features)
         scores = self.w_v(features).squeeze(-1)
         self.attention_weights = masked_softmax(scores, valid_lens)
         return torch.bbm(self.dropout(self.attention_weights), values)
+
+class DotProductAttention(nn.Module):
+    def __init__(self, dropout) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
     
+    def forward(self, queries, keys, values, valid_lens=None):
+        '''
+        shape of queries (batch_size, nof queries, d)
+        shape of queries (batch_size, nof key-value pairs, d)
+        shape of queries (batch_size, nof key-value pairs, value's dimention)
+        '''
+        d = queries.shape[-1]
+        scores = torch.bmm(queries, keys.transpose(1, 2)) / math.sqrt(d)
+        self.attention_weights = masked_softmax(scores, valid_lens)
+        return torch.bmm(self.dropout(self.attention_weights), values)
