582008eed1c12a1c247263ec21f9848cb1af5dad
blameitonme1 <2741122036@qq.com>
2023-12-30 22:57:15 +0800

    finished data iterater's functions, both randomly and sequentially

--- RNN/languageModel.py
+++ RNN/languageModel.py
@@ -15,3 +15,34 @@ trigram_tokens = [triple for triple in zip(
 )]
 trigram_vocab = Vocabulary(trigram_tokens)
 print(trigram_vocab.token_freqs()[:10])
+
+"""randomly generate sequence_data"""
+def seq_data_iter_random(corpus, batch_size, num_steps):
+    corpus = corpus[random.randint(0, num_steps - 1):]
+    """ minus 1 because considering label (Y equals X + 1) """
+    num_subseqs = (len(corpus) - 1) // num_steps
+    intial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    random.shuffle(intial_indices)
+    def data(pos):
+        return corpus[pos : pos + num_steps]
+
+    num_batches = num_subseqs / batch_size
+    for i in range(0, batch_size * num_batches, batch_size):
+        intial_indices_per_batch = intial_indices[i : i + batch_size]
+        X = [data(j) for j in intial_indices_per_batch]
+        Y = [data(j + 1) for j in intial_indices_per_batch]
+        yield torch.tensor(X), torch.tensor(Y)
+
+"""sequentially generate sequence_data"""
+def seq_data_iter_sequential(corpus, batch_size, num_steps):
+    offset = random.randint(0, num_steps)
+    num_tokens = ((len(corpus) - offset - 1) // batch_size) * batch_size
+    Xs = torch.tensor(corpus[offset: offset + num_tokens])
+    Ys = torch.tensor(corpus[offset + 1 : offset + 1 + num_tokens])
+    Xs , Ys = Xs.reshape(batch_size, -1), Ys.reshape(batch_size, -1)
+    num_batches = Xs.shape[1] // num_steps
+    for i in range(0, num_steps * num_batches, num_steps):
+        X = Xs[:, i : i + num_steps]
+        Y = Ys[:, i : i + num_steps]
+        yield X, Y
+
