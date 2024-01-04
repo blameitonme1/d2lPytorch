4d9193208baeef34575ae8cdac240235b2fc16d3
blameitonme1 <2741122036@qq.com>
2024-01-04 23:11:58 +0800

    nmt dataset

---
+++ RNN/machine-translation-dataset.py
@@ -0,0 +1,37 @@
+import torch
+from d2l import torch as d2l
+import os
+#@save
+d2l.DATA_HUB['fra-eng'] = (d2l.DATA_URL + 'fra-eng.zip',
+                           '94646ad1522d915e7b0f9296181140edcf86a4f5')
+def read_data_nmt():
+    data_dir = d2l.download_extract('fra-eng')
+    with open(os.path.join(data_dir, 'fra.txt'), 'r', encoding='utf-8') as f:
+        return f.read()
+
+raw_text = read_data_nmt()
+print(raw_text[:75])
+
+""" preprocessing """
+def preprocess_nmt(text):
+    def no_space(char, prev_char):
+        """ 将标点符号单独拿出来 """
+        return char in set(',.!?') and prev_char != ' '
+    
+    text = text.replace('\u202f', ' ').replace('\xa0', ' ').lower()
+    out = [' ' + char if i > 0 and no_space(char, text[i - 1]) else char
+           for i, char in enumerate(text)]
+    return ' '.join(out)
+text = preprocess_nmt(raw_text)
+print(text[:80])
+def tokenize_nmt(text, num_examples=None):
+    """ num_examples confining the number of tokens"""
+    source, target = [], []
+    for i, line in enumerate(text.split('\n')):
+        if num_examples and i > num_examples:
+            break
+        parts = line.split('\t')
+        if len(parts) == 2:
+            source.append(parts[0].split(' '))
+            target.append(parts[1].split(' '))
+    return source, target
