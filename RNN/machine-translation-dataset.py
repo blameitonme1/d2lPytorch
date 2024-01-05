383aec1aa6f09275effda66a412188ff81277bd5
blameitonme1 <2741122036@qq.com>
2024-01-05 23:42:38 +0800

    NOTICE my vocabulary's implementation should use self.unk() not self.unk!!!!

--- RNN/machine-translation-dataset.py
+++ RNN/machine-translation-dataset.py
@@ -1,6 +1,7 @@
 import torch
 from d2l import torch as d2l
 import os
+import textPreprocessing
 #@save
 d2l.DATA_HUB['fra-eng'] = (d2l.DATA_URL + 'fra-eng.zip',
                            '94646ad1522d915e7b0f9296181140edcf86a4f5')
@@ -9,8 +10,6 @@ def read_data_nmt():
     with open(os.path.join(data_dir, 'fra.txt'), 'r', encoding='utf-8') as f:
         return f.read()
 
-raw_text = read_data_nmt()
-print(raw_text[:75])
 
 """ preprocessing """
 def preprocess_nmt(text):
@@ -21,9 +20,8 @@ def preprocess_nmt(text):
     text = text.replace('\u202f', ' ').replace('\xa0', ' ').lower()
     out = [' ' + char if i > 0 and no_space(char, text[i - 1]) else char
            for i, char in enumerate(text)]
-    return ' '.join(out)
-text = preprocess_nmt(raw_text)
-print(text[:80])
+    """out is now a list, need to transform to a string"""
+    return ''.join(out)
 def tokenize_nmt(text, num_examples=None):
     """ num_examples confining the number of tokens"""
     source, target = [], []
@@ -35,3 +33,42 @@ def tokenize_nmt(text, num_examples=None):
             source.append(parts[0].split(' '))
             target.append(parts[1].split(' '))
     return source, target
+"""ensure the length of the source and target training dataset is SAME"""
+def truncate_pad(line, num_steps, padding_token):
+    if len(line) > num_steps:
+        return line[:num_steps] # truncate
+    return line + [padding_token] * (num_steps - len(line)) # pad
+
+def build_array_nmt(lines, vocab, num_steps):
+    """add eos to each line to signify the end of the senctence."""
+    # 转化成坐标
+    lines = [vocab[l] for l in lines]
+    lines = [l + [vocab['<eos>']] for l in lines]
+    array = torch.tensor(
+        [truncate_pad(l, num_steps, vocab['<pad>']) for l in lines]
+    )
+    # 计算没有填充之前的长度
+    valid_len = (array != vocab['<pad>']).type(torch.int32).sum(1)
+    return array, valid_len
+def load_data_nmt(batch_size, num_steps, num_examples=600):
+    text = preprocess_nmt(read_data_nmt())
+    source, target = tokenize_nmt(text, num_examples)
+    print(source[:6])
+    print(target[:6])
+    src_vocab = textPreprocessing.Vocabulary(source, min_freq=2,
+                                             reserved_tokens=['<pad>', '<bos>', '<eos>'])
+    tgt_vocab = textPreprocessing.Vocabulary(target, min_freq=2,
+                                             reserved_tokens=['<pad>', '<bos>', '<eos>'])
+    src_array, src_valid_len = build_array_nmt(source, src_vocab, num_steps)
+    tgt_array, tgt_valid_len = build_array_nmt(target, tgt_vocab, num_steps)
+    data_arrays = (src_array, src_valid_len, tgt_array, tgt_valid_len)
+    data_iter = d2l.load_array(data_arrays, batch_size)
+    return data_iter, src_vocab, tgt_vocab
+train_iter, src_vocab, tgt_vocab = load_data_nmt(batch_size=2, num_steps=8)
+for X, X_valid_len, Y, Y_valid_len in train_iter:
+    print('X:', X.type(torch.int32))
+    print('X的有效长度:', X_valid_len)
+    print('Y:', Y.type(torch.int32))
+    print('Y的有效长度:', Y_valid_len)
+    break
+    
