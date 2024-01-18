23f8726783145bbaff0b8f5e2c67078d4bd36fd4
blameitonme1 <2741122036@qq.com>
2024-01-18 16:43:44 +0800

    finished all seq2seq model, NOTICE the format when copying code

--- RNN/seq2seq.py
+++ RNN/seq2seq.py
@@ -4,6 +4,7 @@ from d2l import torch as d2l
 import collections
 import math
 from machine_translation_dataset import load_data_nmt
+d2l.Seq2SeqEncoder
 class Seq2SeqEncoder(d2l.Encoder):
     def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0):
         super().__init__()
@@ -31,7 +32,8 @@ class Seq2SeqDecoder(d2l.Decoder):
         return enc_outputs[1] # end_outputs is output, state.
     def forward(self, X, state):
         """ X shape (batch_size,num_steps,embed_size) """
-        X = self.embedding(X).permute(1, 0, 2)
+        X = self.embedding(X)
+        X = X.permute(1, 0, 2)
         """ context is the last layer of the last RNN cell of the encoder """
         context = state[-1].repeat(X.shape[0], 1, 1)
         X_and_context = torch.cat((X, context), 2)
@@ -110,8 +112,62 @@ decoder = Seq2SeqDecoder(len(tgt_vocab), embed_size, num_hiddens, num_layers, dr
 net = d2l.EncoderDecoder(encoder, decoder)
 train_seq2seq(net, train_iter, lr, num_epochs, tgt_vocab, device)
 
+def predict_seq2seq(net, src_sentence, src_vocab, tgt_vocab, num_steps, device, save_attention_weights=False):
+    net.eval()
+    """ 加入<eos>表示句子结尾 """
+    src_tokens = src_vocab[src_sentence.lower().split(' ')] + [src_vocab['<eos>']]
+    enc_valid_len = torch.tensor([len(src_tokens)], device=device)
+    src_tokens = d2l.truncate_pad(src_tokens, num_steps, src_vocab['<pad>']) # 裁剪或者pad句子，根据num_steps
+    # 将原始句子包装成一个张量，dim = 0是为了处理批量 shape of enc_X (1, num_steps)
+    enc_X = torch.unsqueeze(
+        torch.tensor(src_tokens, dtype=torch.long, device=device),dim=0
+    )
+    enc_outputs = net.encoder(enc_X, enc_valid_len) # valid_len现在还用不上，之后会用到
+    dec_state = net.decoder.init_state(enc_outputs, enc_valid_len) # 使用encoder最后一层的hidden state初始化decoder
+    dec_X = torch.unsqueeze(
+        torch.tensor([tgt_vocab['<bos>']], dtype=torch.long, device=device), dim=0
+    )
+    output_seq, attention_weight_seq = [], []
+    for _ in range(num_steps):
+        # 最多预测num_steps个词
+        Y, dec_state = net.decoder(dec_X, dec_state)
+        # 取预测的最大可能性的词作为下一个时间步的输入
+        dec_X = Y.argmax(dim=2)
+        pred = dec_X.squeeze(dim=0).type(torch.int32).item()
+        # 注意力机制，现在暂时不用
+        if save_attention_weights:
+            attention_weight_seq.append(net.decoder.attention_weights)
+        if pred == tgt_vocab['<eos>']:
+            # 预测到了句子结尾
+            break        
+        output_seq.append(pred)
+    return ' '.join(tgt_vocab.to_tokens(output_seq)), attention_weight_seq
+
+def bleu(pred_seq, label_seq, k):
+    """计算 BLEU, 直接抄公式即可 """
+    pred_tokens, label_tokens = pred_seq.split(' '), label_seq.split(' ')
+    len_pred, len_label = len(pred_tokens), len(label_tokens)
+    score = math.exp(min(0, 1 - len_label / len_pred)) # 惩罚过短的预测
+    # 统计n gram的准确率
+    for n in range(1, k + 1):
+        num_mathes, label_subs = 0, collections.defaultdict(int)
+        for i in range(len_label - n + 1):
+            label_subs[' '.join(label_tokens[i : i + n])] += 1
+        for i in range(len_pred - n + 1):
+            if label_subs[' '.join(pred_tokens[i : i + n])] > 0:
+                # label含有这个n gram
+                num_mathes += 1
+                label_subs[' '.join(pred_tokens[i : i + n])] -= 1
+        score *= math.pow(num_mathes / (len_pred - n + 1), math.pow(0.5, n))
+    return score
 
 
+engs = ['go .', "i lost .", 'he\'s calm .', 'i\'m home .']
+fras = ['va !', 'j\'ai perdu .', 'il est calme .', 'je suis chez moi .']
+for eng, fra in zip(engs, fras):
+    translation, attention_weight_seq = predict_seq2seq(
+        net, eng, src_vocab, tgt_vocab, num_steps, device)
+    print(f'{eng} => {translation}, bleu {bleu(translation, fra, k=2):.3f}')
 
 
 
