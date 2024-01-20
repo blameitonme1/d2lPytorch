3677fd27a4aec176f446cbdaecc84dd9c7b90560
blameitonme1 <2741122036@qq.com>
2024-01-20 17:30:02 +0800

    finished transformer's implementation, and trained it and use it to predict some English to France MT

--- Attention-machanism/Multihead-attention.py
+++ Attention-machanism/Multihead-attention.py
@@ -123,3 +123,115 @@ class TransformerEncoder(d2l.Encoder):
             X = blk(X, valid_lens)
             self.attention_weights[i] = blk.attention.attention.attention_weights
         return X
+
+class DecoderBlock(nn.Module):
+    def __init__(self, key_size, query_size, value_size, num_hiddens,
+                 norm_shape, ffn_num_input, ffn_num_hiddens, num_heads,
+                 dropout, i) -> None:
+        super().__init__()
+        # 存放编号，因为要根据编号选择编码器的输出序列之一作为第二个attention的key和value
+        self.i = i
+        # 同样使用lazy Linear不需要指定输入维度
+        self.attention1 = d2l.MultiHeadAttention(num_hiddens, num_heads, dropout)
+        self.addnorm1 = AddNorm(norm_shape, dropout)
+        self.attention2 = d2l.MultiHeadAttention(num_hiddens, num_heads, dropout)
+        self.addnorm2 = AddNorm(norm_shape, dropout)
+        self.ffn = PositionWiseFFN(ffn_num_input, ffn_num_hiddens, num_hiddens)
+        self.addnorm3 = AddNorm(norm_shape, dropout)
+    
+    def forward(self, X, state):
+        # 训练阶段，输出序列的所有词元都在同一时间处理，
+        # 因此state[2][self.i]初始化为None。
+        # 预测阶段，输出序列是通过词元一个接着一个解码的，
+        # 因此state[2][self.i]包含着直到当前时间步第i个块解码的输出表示
+        """ 这里将编码器的输出序列作为第二个multihead attention的key和value,就是transformer的信息传递部分 """
+        enc_outputs, enc_valid_lens = state[0], state[1]
+        if state[2][self.i] is None:
+            # 训练模式，使用自注意力
+            key_values = X
+        else:
+            # 预测模式，所有的keyvalue都是之前预测得到的序列
+            key_values = torch.cat((state[2][self.i], X), axis=1)
+        
+        state[2][self.i] = key_values
+        if self.training:
+            batch_size, num_steps, _ = X.shape
+            # dec_valid_lens的开头:(batch_size,num_steps),
+            # 其中每一行是[1,2,...,num_steps]
+            dec_valid_lens = torch.arange(
+                1, num_steps + 1, device=X.device).repeat(batch_size, 1)
+        
+        else:
+            # 预测的时候不知道多少是valid_lens
+            dec_valid_lens = None
+        # 自注意力
+        X2 = self.attention1(X, key_values, key_values, dec_valid_lens)
+        Y = self.addnorm1(X, X2)
+        Y2 = self.attention2(Y, enc_outputs, enc_outputs, enc_valid_lens)
+        Z = self.addnorm2(Y, Y2)
+        return self.addnorm3(Z, self.ffn(Z)), state
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, vocab_size, key_size, query_size, value_size,
+                 num_hiddens, norm_shape, ffn_num_input, ffn_num_hiddens,
+                 num_heads, num_layers, dropout):
+        super().__init__()
+        self.num_hiddens = num_hiddens
+        self.num_layers = num_layers
+        # 输出X的形状是 (batch_size, numOfSequence(处理文本就是num_steps), num_hiddens(也就是单个x的长度))
+        self.embedding = nn.Embedding(vocab_size, num_hiddens)
+        self.pos_encoding = d2l.PositionalEncoding(num_hiddens, dropout)
+        self.blks = nn.Sequential()
+        for i in range(num_layers):
+            self.blks.add_module("block"+str(i),
+                                 DecoderBlock(key_size, query_size, value_size, num_hiddens,
+                             norm_shape, ffn_num_input, ffn_num_hiddens,
+                             num_heads, dropout, i))
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+
+    def init_state(self, enc_outputs, enc_valid_lens):
+        # 初始化上面的state
+        return [enc_outputs, enc_valid_lens, [None] * self.num_layers]
+    
+    def forward(self, X, state):
+        X = self.pos_encoding(self.embedding(X) * math.sqrt(self.num_hiddens))
+        self._attention_weights = [[None] * len(self.blks) for _ in range(2)]
+        for i, blk in enumerate(self.blks):
+            X, state = blk(X, state)
+            # 分别储存每一层的两个attention的权重
+            self._attention_weights[0][
+                i] = blk.attention1.attention.attention_weights
+            self._attention_weights[1][i] = blk.attention2.attention.attention_weights
+        
+        return self.dense(X), state
+    
+    def attention_weights(self):
+        return self._attention_weights
+
+num_hiddens, num_layers, dropout, batch_size, num_steps = 32, 2, 0.1, 64, 10
+lr, num_epochs, device = 0.005, 200, d2l.try_gpu()
+ffn_num_input, ffn_num_hiddens, num_heads = 32, 64, 4
+# 反正都是自注意力机制，size都是一样的
+key_size, query_size, value_size = 32, 32, 32
+# 层归一化作用在最后一个维度，大小为32, 也就是将每一个X的自己内部归一化就行
+norm_shape = [32]
+
+train_iter, src_vocab, tgt_vocab = d2l.load_data_nmt(batch_size, num_steps)
+encoder = TransformerEncoder(
+    len(src_vocab), key_size, query_size, value_size, num_hiddens,
+    norm_shape, ffn_num_input, ffn_num_hiddens, num_heads,
+    num_layers, dropout)
+decoder = TransformerDecoder(
+    len(tgt_vocab), key_size, query_size, value_size, num_hiddens,
+    norm_shape, ffn_num_input, ffn_num_hiddens, num_heads,
+    num_layers, dropout)
+net = d2l.EncoderDecoder(encoder, decoder)
+d2l.train_seq2seq(net, train_iter, lr, num_epochs, tgt_vocab, device)
+
+engs = ['go .', "i lost .", 'he\'s calm .', 'i\'m home .']
+fras = ['va !', 'j\'ai perdu .', 'il est calme .', 'je suis chez moi .']
+for eng, fra in zip(engs, fras):
+    translation, dec_attention_weight_seq = d2l.predict_seq2seq(
+        net, eng, src_vocab, tgt_vocab, num_steps, device, True)
+    print(f'{eng} => {translation}, ',
+          f'bleu {d2l.bleu(translation, fra, k=2):.3f}')
